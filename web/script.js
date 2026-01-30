b1bce0ce69e0a6d3d19b281e5dfb43233d66f757
Samuel Alejandro <samuel.avera@estudiantes.matcom.uh.cu>
2026-01-30 08:00:27 -0500

    He agregado un botón para introducir una clave secreta y he estructurado mejor mis ideas para el desarrollo del servidor

--- web/script.js
+++ web/script.js
@@ -14,12 +14,16 @@ const divRegistro = document.getElementById('divRegistro')
 const btnCambiarRegistroInicio = document.getElementById('btnCambiarRegistroInicio')
 const nombreUsuarioRegistro = document.getElementById('inputNombreUsuarioRegistro')
 const password1Registro = document.getElementById('inputContr1Registro')
-const password2Registro = document.getElementById('inputContr2Registro')
 const errorRegistroClave = document.getElementById('errorRegistroClave')
 const errorRegistroUsuario = document.getElementById('errorRegistroUsuario')
 const condicionesUsuario = document.querySelectorAll('h3')
 const btnRegistro = document.getElementById('btnAceptarRegistro')
 const condicionesClave = errorRegistroClave.querySelectorAll('h3')
+const claveSecreta = document.getElementById('claveSecreta')
+/*Div Repetir Clave */
+const divRepetirClave = document.getElementById('divRepetirClave')
+const btnCancelarRepetirClave = document.getElementById('btnCancelarRepetirClave')
+const password2Registro = document.getElementById('inputContr2Registro')
 
 /*Variables Js */
 let cantidadDeCaracteresClave = false
@@ -28,7 +32,6 @@ let contieneMayusculas = false
 let contieneEnteros = false
 let valoresPermitidosClave = true
 let valoresPermitidosUsuario = true
-let clavesIguales = true
 const valoresPermitidos = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz._"    
 
 /*Funcionalidades botones div bienvenida*/
@@ -57,38 +60,41 @@ btnCambiarRegistroInicio.addEventListener('click', ()=>{
     divInicioSes.classList.remove('elementoTransparente')
 })
 
-/*Definiendo una función que le pasas unos valores y un str y te devuelve si el str tiene o no los valores */
-function verificarValores(valoresPermitidos, valor, n){ 
-    /*n=true Verifica un valor esté*/
-    if(n){
-        for(const i of valor){
-            if(valoresPermitidos.includes(i)){
-                return true
-            }
-        }
-        return false
+
+/*Validar intento de registro: */
+btnRegistro.addEventListener('click', ()=>{
+    if(
+        cantidadDeCaracteresClave && cantidadDeCaracteresUsuario 
+        && contieneMayusculas && contieneEnteros 
+        && valoresPermitidosClave && valoresPermitidosUsuario 
+        &&  claveSecreta.value.length > 7
+    ){
+        divRepetirClave.style.visibility = 'visible'
+        divRepetirClave.style.filter = 'opacity(100%)'
     }
-    /*n = false Verifica que todos los valores estén */
+})
 
-    for(const i of valor){
-        if(!valoresPermitidos.includes(i)){
-            return false
-        }
-    }
-    return true
-}
+/*Salir de repetir clave */
+btnCancelarRepetirClave.addEventListener('click', ()=>{
+    divRepetirClave.style.filter = 'opacity(0%)'
+    setTimeout(()=>{divRepetirClave.style.visibility = 'hidden'
+        console.log('se ejecuta')
+    }, 600)
+})
 
 
-function validarClave(clave, segundaClave, valoresPermitidos){
-    const resultados = [true, true, false, false, false] /*0: cantidad de caracteres, 1: valores permitidos, 2: mayúscula, 3:entero, 4: claves iguales*/
+
+
+
+
+
+
+function validarClave(clave, valoresPermitidos){
+    const resultados = [true, true, false, false] /*0: cantidad de caracteres, 1: valores permitidos, 2: mayúscula, 3:entero*/
     if(clave.length<8 || clave.length>25){
         resultados[0] = false
     }
     
-    if (clave == segundaClave){
-        resultados[4] = true
-    }
-
     for(const i of clave){
         if (valoresPermitidos.slice(0, 26).includes(i)){
             resultados[2] = true
@@ -108,26 +114,6 @@ function validarClave(clave, segundaClave, valoresPermitidos){
     return resultados
 }
 
-/*Verificar que esté bien el slice
-console.log(valoresPermitidos.slice(0, 26),
-valoresPermitidos.slice(26, 36),
-valoresPermitidos.slice(36, 64))
-*/
-
-
-/*Probar que funcione correctamente la función*/
-/*console.log(
-    validarClave('A1hol...','A1hol...', valoresPermitidos),
-    validarClave('AAdsfsdn', 'AAdsfsdn', valoresPermitidos),
-    validarClave('11dsfsdn', '11dsfsdno', valoresPermitidos),
-    validarClave('','', valoresPermitidos),
-    validarClave('A1hol ...','A1hol ...', valoresPermitidos),
-    validarClave('hol','hol', valoresPermitidos),
-    validarClave('1hol','hol', valoresPermitidos),
-    validarClave('1hol','1hol', valoresPermitidos),
-    validarClave('Ahol','Ahol', valoresPermitidos),
-    validarClave('A1hol.........................................','A1hol...............................................', valoresPermitidos),
-)*/
 
 function validarUsuario(usuario, valoresPermitidos){
     const resultados = [true, true] /*0: cantidad de caracteres, 1: valores permitidos */
@@ -145,37 +131,29 @@ function validarUsuario(usuario, valoresPermitidos){
     return resultados
 }
 
-/*Probar que funcione correctamente la función
-console.log(validarUsuario('88888888', valoresPermitidos))
-console.log(validarUsuario('8888888', valoresPermitidos))
-console.log(validarUsuario('88888888888888888888888888888888888888888888888888888888888888888', valoresPermitidos))
-console.log(validarUsuario('Samu elale', valoresPermitidos))
-console.log(validarUsuario('Samuelale88888', valoresPermitidos))
-*/
-
 
 /*Validación de registro de usuario*/
 /*Validar la clave introducida*/
 password1Registro.addEventListener('input', ()=>{
-    const resultados = validarClave(password1Registro.value, password2Registro.value, valoresPermitidos)
+    const resultados = validarClave(password1Registro.value, valoresPermitidos)
     if(cantidadDeCaracteresClave && !resultados[0]){
-        condicionesClave[0].style.color = 'red'
+        condicionesClave[1].style.color = 'red'
         cantidadDeCaracteresClave = false
     }
     else if(resultados[0]){
         cantidadDeCaracteresClave = true
-        condicionesClave[0].style.color = 'hsl(273, 100%, 50%)'
+        condicionesClave[1].style.color = 'hsl(273, 100%, 50%)'
     }
 
     if(!resultados[1]){
         if (valoresPermitidosClave){
-            condicionesClave[1].style.color = 'red'
+            condicionesClave[0].style.color = 'red'
             valoresPermitidosClave = false
         }
     }
 
     else if(!valoresPermitidosClave){
-        condicionesClave[1].style.color = 'hsl(273, 100%, 50%)'
+        condicionesClave[0].style.color = 'hsl(273, 100%, 50%)'
         valoresPermitidosClave = true
     }
 
@@ -200,31 +178,8 @@ password1Registro.addEventListener('input', ()=>{
     condicionesClave[3].style.color = 'red'
     contieneEnteros = false
     }
-
-    if(!resultados[4]){
-        if(clavesIguales){
-            condicionesClave[4].style.color = 'red'
-            clavesIguales = false
-        }
-    }
-    else if(!clavesIguales){
-        condicionesClave[4].style.color = 'hsl(273, 100%, 50%)'
-        clavesIguales = true
-    }
 })
 
-password2Registro.addEventListener('input',()=>{
-    if(password1Registro.value!=password2Registro.value){
-        if(clavesIguales){
-            condicionesClave[4].style.color = 'red'
-            clavesIguales = false
-        }
-    }
-    else if(!clavesIguales){
-        condicionesClave[4].style.color = 'hsl(273, 100%, 50%)'
-        clavesIguales = true
-    }
-})
 
 /*Validar el usuario introducido*/
 nombreUsuarioRegistro.addEventListener('input', ()=>{
@@ -257,11 +212,10 @@ nombreUsuarioRegistro.addEventListener('input', ()=>{
 /*Validación de intento de inicio de sesión*/
 btnInicioSes.addEventListener('click', ()=>{
     const clave = passwordInicio.value
-    const resultadosClave = validarClave(clave, clave, valoresPermitidos)
-    const resultadosUsuario = validarUsuario(nombreDeUsuarioInicio.value, valoresPermitidos)
+    const resultadosClave = validarClave(clave)
+    const resultadosUsuario = validarUsuario(nombreDeUsuarioInicio.value)
     let resultadoFinal = true
-    console.log('clave:',clave)
-    console.log(resultadosClave, 'resultados clave', resultadosUsuario, 'resultados usuario')
+    
     for(const i of resultadosClave){
         if(!i){
             resultadoFinal = false
@@ -278,7 +232,7 @@ btnInicioSes.addEventListener('click', ()=>{
     }
 
     if(resultadoFinal){
-        
+        console.log('inicio de sesi')
     }
     else{
         console.log('usuario o contraseña incorrectos')
@@ -289,7 +243,18 @@ btnInicioSes.addEventListener('click', ()=>{
 /*Definir una solicitud fetch para recibir y enviar los datos*/
 
 /*Solicitud de registro */
-async function registrarUsuario(usuario, clave){
+password2Registro.addEventListener('input', ()=>{
+    if(password2Registro.value == password1Registro.value){
+        divRepetirClave.style.filter = "opacity(0%)"
+        setTimeout(()=>{
+            divRepetirClave.style.visibility = 'hidden'
+        }, 600)
+        return console.log('se ha enviado una solicitud fetch') /************************************************************* */
+    }
+})
+
+
+async function registrarUsuario(usuario, clave, claveSecreta){
     try{
         const respuesta = await fetch(`http://localhost:5000/registro`, {
             method: 'POST',
@@ -298,7 +263,8 @@ async function registrarUsuario(usuario, clave){
             },
             body: JSON.stringify({
                 "usuario" : usuario,
-                "clave" : clave
+                "clave" : clave, 
+                "claveSecreta" : claveSecreta
             })
         })
         if (!respuesta.ok){
@@ -319,37 +285,7 @@ async function registrarUsuario(usuario, clave){
     
 
 
-/*Solicitar los datos de una persona en una fecha específica*/
-async function obtenerDatosFecha(fecha, usuario, clave){
-    try{
-        const respuesta = await fetch(`http://localhost:5000/datosFecha/${fecha}/${usuario}/${clave}`)
-        if (!respuesta.ok){
-            throw new Error('Error en la red')
-        }{
-            const datos = await response.json()
-            return datos
-        }
-    }
-    catch(error){
-        console.log(errorAlObtenerLosDatos)
-    }
-}
-/*Solicitar todos los datos de una persona*/
-async function obtenerDatosGeneral(usuario, clave){
-    try{
-        const respuesta = await fetch(`http://localhost:5000/datosGeneral/${usuario}/${clave}`)
-        if (!respuesta.ok){
-            throw new Error('Error en la red')/************************************************ */
-        }{
-            const datos = await response.json()
-            return datos /**************************************************************** */
-        }
-    }
-    catch(error){
-        throw(error)/**************************************************************** */
-    }
-}
-
+/*Ejecutar el efecto de partículas */
 particlesJS('particles-js', {
     particles: {
         number: {
@@ -451,4 +387,4 @@ particlesJS('particles-js', {
         }
     },
     retina_detect: true
-});
+})
